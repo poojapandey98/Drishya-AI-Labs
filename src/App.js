4f18728c2d61d7b597d5d0c4ccf8e6c95b123878
Pooja Pandey <poojapandey8890@gmail.com>
2022-09-16 23:09:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useState } from "react";
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const App = () => {
+  const [rows, setRows] = useState([{}]);
+  const columnsArray = ["1", "2", "3"];
+
+  const handleAddRow = () => {
+    const item = {};
+    setRows([...rows, item]);
+  };
+
+  const handleRemoveSpecificRow = (idx) => {
+    const tempRows = [...rows];
+    tempRows.splice(idx, 1);
+    setRows(tempRows);
+  };
+
+  const updateState = (e) => {
+    let prope = e.target.attributes.column.value;
+    let index = e.target.attributes.index.value;
+    let fieldValue = e.target.value;
+
+    const tempRows = [...rows];
+    const tempObj = rows[index];
+    tempObj[prope] = fieldValue;
+
+    tempRows[index] = tempObj;
+    setRows(tempRows);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">
+        <div className="row clearfix">
+          <div className="col-md-12 column">
+            <table className="table table-bordered table-hover" id="tab_logic">
+              <thead>
+                <tr>
+                  <th className="text-center"> # </th>
+                  {columnsArray.map((column, index) => (
+                    <th className="text-center" key={index}>
+                      {column}
+                    </th>
+                  ))}
+                  <th />
+                </tr>
+              </thead>
+              <tbody>
+                {rows.map((item, idx) => (
+                  <tr key={idx}>
+                    <td>{idx + 1}</td>
+                    
+                    {columnsArray.map((column, index) => (
+                      <td key={index}>
+                        <input
+                          type="text"
+                          column={column}
+                          value={rows[idx][column]}
+                          index={idx}
+                          className="form-control"
+                          onChange={(e) => updateState(e)}
+                        />
+                      </td>
+                    ))}
+
+                    <td>
+                      <button
+                        className="btn btn-outline-danger btn-sm"
+                        onClick={() => handleRemoveSpecificRow(idx)}
+                      >
+                        Remove
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <button onClick={handleAddRow} className="btn btn-primary">
+              Add Row
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
+
+
+
